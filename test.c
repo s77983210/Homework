922caaa22e6c7413465b2cf9e021df4c7dc56a91
何書齊 <ho.ericsc@inventec.com>
2020-11-16 10:29:41 +0800

    init

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main ()
+{
+    int a = 10;
+
+    printf("%i\n", abc(a));
+
+    return 0; 
+}
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#ifdef __cplusplus
+extern "C"
+#endif
+
+int abc(int a)
+{
+    return a;
+}
+
+/*int main(int argc, char **argv)
+{
+}*/
